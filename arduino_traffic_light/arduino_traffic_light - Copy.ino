03f1cae6e82aaeee899ae60101ed6bd8a24baf97
Fib0112358 <wmwallace5@gmail.com>
2016-01-29 10:13:55 -0600

    213

---
+++ arduino_traffic_light/arduino_traffic_light - Copy.ino
@@ -0,0 +1,76 @@
+//Author: wmwallace+ oprice
+
+#define   LED_1   6
+#define   LED_2   10
+#define   LED_3   11
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_1, HIGH);
+  delay(500);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(500);
+  digitalWrite(LED_2, LOW);
+  digitalWrite(LED_3, HIGH);
+  delay(500);
+  digitalWrite(LED_3, LOW); 
+  delay(500);
+  digitalWrite(LED_1, HIGH);
+  delay(500);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(500);
+  digitalWrite(LED_2, LOW);
+  digitalWrite(LED_3, HIGH);
+  delay(500);
+  digitalWrite(LED_3, LOW);
+  delay(500);
+  digitalWrite(LED_3, HIGH);
+  delay(250);
+  digitalWrite(LED_3, LOW);
+  delay(10);
+  digitalWrite(LED_3, HIGH);
+  delay(250);
+  digitalWrite(LED_3, LOW);
+  delay(10);
+  digitalWrite(LED_3, HIGH);
+  delay(250);
+  digitalWrite(LED_3, LOW);
+  delay(10);
+  digitalWrite(LED_3, HIGH);
+  delay(250);
+  digitalWrite(LED_3, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(250);
+  digitalWrite(LED_2, LOW);
+  delay(10);
+  digitalWrite(LED_2, HIGH);
+  delay(250);
+  digitalWrite(LED_2, LOW);
+  delay(10);
+  digitalWrite(LED_2, HIGH);
+  delay(250);
+  digitalWrite(LED_2, LOW);
+  delay(10);
+  digitalWrite(LED_2, HIGH);
+  delay(250);
+  digitalWrite(LED_2, LOW);
+  digitalWrite(LED_1, HIGH);
+  delay(500);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(500);
+  digitalWrite(LED_2, LOW);
+  digitalWrite(LED_3, HIGH);
+  delay(500);
+  digitalWrite(LED_3, LOW);
+  delay(5000);
+ }
